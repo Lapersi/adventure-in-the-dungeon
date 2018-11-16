96a49e6cc9d1338497c9ab4248f15ef9d7fd6bf9
Daniel Delgado Corona <ddelgadoc@hotmail.com>
2018-11-15 20:53:27 -0600

    Adventure in the Dungeon v 0.0.2.a
    Main menu desing and transition to the gameplay scene,  firts interaction added.

---
+++ Adventure in the Dungeon/Assets/Scripts/ChanceScenes.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChanceScenes : MonoBehaviour {
+
+    public  void ChanceScene(string scene)
+    {
+        SceneManager.LoadScene(scene);
+    }
+}
