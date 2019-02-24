aaeb5f6314288a33d1c2a308e61bba294fd6d29a
Daniel Delgado Corona <ddelgadoc@hotmail.com>
2019-02-23 23:36:03 -0600

    Paused Menu Funtions
    Funtions of the Pause Menu implement exect for save, save & quit.

---
+++ Adventure in the Dungeon/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour {
+
+    public static bool GameIsPause = false;
+
+    public GameObject pauseMenuUI;
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Escape) || Input.GetKeyDown(KeyCode.P))
+        {
+           if(GameIsPause)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+	}
+
+    void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPause = true;
+    }
+
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPause = false;
+    }
+
+    public void NewGame()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("Dungeon");
+    }
+
+    public void MainMenu()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("Main Menu");
+    }
+
+    public void Options()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("Options");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
